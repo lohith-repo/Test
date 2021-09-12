923ae37138770440407d86f61ee699a08432f8ff
Lohith Nagaraja <lohithnagaraja@Lohiths-iMac.local>
2021-09-12 14:17:55 -0700

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+
+
+        int[] inputNumbers = new int[] { 2454,4, 1, 565, 231};
+
+        for(int no:inputNumbers) {
+            System.out.println("The entry is " + no);
+        }
+
+
+
+        Arrays.sort(inputNumbers);
+
+        for(int no:inputNumbers) {
+            System.out.println("The entry is " + no);
+        }
+    }
+}
